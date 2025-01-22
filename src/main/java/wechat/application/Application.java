a77efac722f602fe7cef9caee77b4cb00e682a2d
Chen Haoquan <chenhaoquanchenhaoquan@gmail.com>
2025-01-22 09:32:20 +0700

    fix: controller

--- src/main/java/wechat/application/Application.java
+++ src/main/java/wechat/application/Application.java
@@ -2,8 +2,10 @@ package wechat.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "wechat.controller")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
