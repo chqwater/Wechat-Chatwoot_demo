1810e5e4bc7375be2e958ca3ea4ee797970f001c
Chen Haoquan <chenhaoquanchenhaoquan@gmail.com>
2025-01-22 09:37:11 +0700

    fix: wechat verify

--- src/main/java/wechat/Application.java
+++ src/main/java/wechat/Application.java
@@ -1,11 +1,9 @@
-package wechat.application;
+package wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "wechat.controller")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
